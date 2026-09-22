4b380d5e80955bf8ee644be0ba8f78618bb660de
lefonde <alexlef3@gmail.com>
2026-09-22 10:02:15 +0300

    Enable approved next-study recommendations on the main site

--- docs/config.js
+++ docs/config.js
@@ -1,1 +1,1 @@
-window.STUDY_CONFIG = Object.freeze({mode: "static", storageKey: "matzpen:22902:2026-s3:progress:v1", learningPilot: true, reportImport: true, scaffoldPilot: true, mixedPilot: true, reviewPilot: true, recommendationPilot: false});
+window.STUDY_CONFIG = Object.freeze({mode: "static", storageKey: "matzpen:22902:2026-s3:progress:v1", learningPilot: true, reportImport: true, scaffoldPilot: true, mixedPilot: true, reviewPilot: true, recommendationPilot: true});
